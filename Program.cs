69e298bd79367fa11112b213ab10c7c1540ab9d3
Matthew James <matthew.james@zupatech.co.uk>
2020-07-01 17:03:29 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace ComparableLinqMethodsBenchmark
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            BenchmarkRunner.Run<SelectContainsVsAnyBenchmark>();
+        }
+    }
+}
---
+++ SelectContainsVsAnyBenchmark.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Linq;
+
+namespace ComparableLinqMethodsBenchmark
+{
+    public class SelectContainsVsAnyBenchmark
+    {
+        private Guid _id;
+        private SomeModel[] _source;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _id = Guid.NewGuid();
+            _source = new[]
+            {
+                new SomeModel { Id = Guid.NewGuid() },
+                new SomeModel { Id = _id },
+                new SomeModel { Id = Guid.NewGuid() }
+            };
+        }
+
+        [Benchmark]
+        public bool BenchmarkSelectContains() => _source.Select(m => m.Id).Contains(_id);
+
+        [Benchmark]
+        public bool BenchmarkAny() => _source.Any(m => m.Id == _id);
+
+        private class SomeModel
+        {
+            public Guid Id { get; set; }
+        }
+    }
+}
