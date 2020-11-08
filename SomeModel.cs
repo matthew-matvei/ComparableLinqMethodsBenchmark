7b2e6ef4bb8540669812b25a4866cb8d1addd45d
matthew-matvei <matthew.d.james87@gmail.com>
2020-11-08 13:42:35 +1000

    Extend models

--- SomeModel.cs
+++ SomeModel.cs
@@ -7,11 +7,17 @@ namespace ComparableLinqMethodsBenchmark
     public class SomeModel
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public DateTimeOffset When { get; set; }
 
         public static IEnumerable<SomeModel> Generate(uint count) =>
             new Faker<SomeModel>()
                 .StrictMode(true)
                 .RuleFor(m => m.Id, _ => Guid.NewGuid())
+                .RuleFor(m => m.Name, f => f.Name.FirstName())
+                .RuleFor(m => m.Value, f => f.Random.Int(0, 100))
+                .RuleFor(m => m.When, f => f.Date.SoonOffset())
                 .GenerateLazy((int)count);
     }
 }
